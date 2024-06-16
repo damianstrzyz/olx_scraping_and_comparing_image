19faac45e08fc9063b8b20cc1bd76209b34d7068
Damian Strzyż <138368043+damianstrzyz@users.noreply.github.com>
2024-06-16 18:10:02 +0200

    Update main.py
    v4 - wrong direction

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-
 import cv2
-import numpy as np
+import matplotlib.pyplot as plt
 
-def confidence(img, template):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-    res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
-    conf = res.max()
-    return np.where(res == conf), conf
+# Read images
+examples = [cv2.imread(img) for img in ['./szukanie_olx/opencv_images/ex1.png', './szukanie_olx/opencv_images/ex2.png', './szukanie_olx/opencv_images/ex3.png', './szukanie_olx/opencv_images/ex4.png', './szukanie_olx/opencv_images/ex5.png']]
+target = cv2.imread('./szukanie_olx/dane/sample.png')
+h, w = target.shape[:2]
 
-files = ["./szukanie_olx/opencv_images/ex7.png", "./szukanie_olx/opencv_images/ex8.png", "./szukanie_olx/opencv_images/ex9.png", "./szukanie_olx/opencv_images/ex10.png", "./szukanie_olx/opencv_images/ex11.png", "./szukanie_olx/opencv_images/ex12.png", "./szukanie_olx/opencv_images/ex13.png", "./szukanie_olx/opencv_images/ex14.png", "./szukanie_olx/opencv_images/ex15.png"]
+# Iterate examples
+for i, img in enumerate(examples):
 
-template = cv2.imread("./szukanie_olx/dane/sampley.png")
-h, w, _ = template.shape
+    # Template matching
+    # cf. https://docs.opencv.org/4.5.2/d4/dc6/tutorial_py_template_matching.html
+    res = cv2.matchTemplate(img, target, cv2.TM_CCOEFF_NORMED)
 
-for name in files:
-    img = cv2.imread(name)
-    ([y], [x]), conf = confidence(img, template)
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    text = name[-8:] + f' : {round(float(conf), 2)}'
-    # cv2.putText(img, text, (x, y), 1, cv2.FONT_HERSHEY_PLAIN, (0, 0, 0), 2)
-    # cv2.imshow(name, img)
-    print (text)
-# cv2.imshow('Template', template)
-# cv2.waitKey(0)
+    # Get location of maximum
+    _, max_val, _, top_left = cv2.minMaxLoc(res)
+
+    # Set up threshold for decision target found or not
+    thr = 0.7
+    if max_val > thr:
+
+        # Show found target in example
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+        cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 2)
+
+    # Visualization
+    plt.figure(i, figsize=(10, 5))
+    plt.subplot(1, 2, 1), plt.imshow(img[..., [2, 1, 0]]), plt.title('Example')
+    plt.subplot(1, 2, 2), plt.imshow(res, vmin=0, vmax=1, cmap='gray')
+    plt.title('Matching result'), plt.colorbar(), plt.tight_layout()
+
+plt.show()
